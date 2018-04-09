cce544b983d35d4cb4b4547c3600178965fdd4a4
Moises Gil <moisesgilsantana@gmail.com>
2018-04-08 22:21:06 -0400

    Setting frontend enviroment done

--- src/main/java/com/mgil/courses/pluralsight/bookstore/model/Language.java
+++
@@ -1,5 +0,0 @@
-package com.mgil.courses.pluralsight.bookstore.model;
-
-public enum Language {
-    ENGLISH, FRENCH, SPANISH, PORTUGUESE, ITALIAN, FINISH, GERMAN, DEUTSCH, RUSSIAN
-}
--- src/main/java/com/mgil/courses/pluralsight/bookstore/util/NumberGenerator.java
+++
@@ -1,5 +0,0 @@
-package com.mgil.courses.pluralsight.bookstore.util;
-
-public interface NumberGenerator {
-    String generateNumber();
-}
